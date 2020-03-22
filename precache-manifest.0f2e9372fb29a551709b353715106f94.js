a424a53b5530b333ccdb5cdb834c48d000ef7084
Kush Garg <kushg02@gmail.com>
2020-03-21 17:07:12 -0700

    Updates

---
+++ precache-manifest.0f2e9372fb29a551709b353715106f94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f181a150fb38d74537194bd9f6dc1b3f",
+    "url": "/mern-react-todo/index.html"
+  },
+  {
+    "revision": "b2e52c86ea44e824a9f8",
+    "url": "/mern-react-todo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c4cbda47c4baa2960275",
+    "url": "/mern-react-todo/static/css/main.cbedd9ce.chunk.css"
+  },
+  {
+    "revision": "b2e52c86ea44e824a9f8",
+    "url": "/mern-react-todo/static/js/2.9bb9590f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/mern-react-todo/static/js/2.9bb9590f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4cbda47c4baa2960275",
+    "url": "/mern-react-todo/static/js/main.842d14c7.chunk.js"
+  },
+  {
+    "revision": "9a922e53c5668d238c0a",
+    "url": "/mern-react-todo/static/js/runtime-main.5e2170f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mern-react-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
